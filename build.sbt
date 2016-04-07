cae034b8f4f8747fdb6e5e6a8de10ed926ae8443
Cloud Tang <tangyun23@gmail.com>
2016-04-07 14:55:49 +0800

    update sparse MM, promote spark version

--- build.sbt
+++ build.sbt
@@ -4,7 +4,7 @@ version := "1.0"
 
 scalaVersion := "2.10.4"
 
-libraryDependencies +=  "org.apache.spark" % "spark-mllib_2.10" % "1.4.0"
+libraryDependencies +=  "org.apache.spark" % "spark-mllib_2.10" % "1.5.2"
 
 resolvers += Resolver.mavenLocal
 
--- src/main/scala/org/apache/spark/mllib/MllibMatrix.scala
+++ src/main/scala/org/apache/spark/mllib/MllibMatrix.scala
@@ -7,14 +7,11 @@ import org.apache.spark.mllib.linalg.distributed.{BlockMatrix, IndexedRow, Index
 import org.apache.spark.mllib.linalg.{Matrix, Vectors}
 import org.apache.spark.{SparkConf, SparkContext}
 
-/**
- * Created by Cloud on 2015-07-08.
- */
 object MllibMatrix {
   def main(args: Array[String]) {
     if (args.length < 5) {
-      println("usage: MllibMatrix <matA rows> <matA cols> <matB cols> " +
-        "<sub-block row> <sub-block col>")
+      println("usage: org.apache.spark.mllib.MllibMatrix <matA rows> <matA cols> <matB cols> " +
+        "<sub-block row> <sub-block col> <parallelism>")
       System.exit(1)
     }
     val rowsA = args(0).toInt
@@ -24,16 +21,16 @@ object MllibMatrix {
     val subCol = args(4).toInt
     val partitionNumA = rowsA / subRow
     val partitionNumB = rowsA / subRow
-    val parallesim = if (args.length > 5) { args(5).toInt } else 192
+    val parallelism = if (args.length > 5) { args(5).toInt } else 320
     val conf = new SparkConf()
     conf.registerKryoClasses(Array(classOf[IndexedRow], classOf[BlockMatrix], classOf[Matrix]))
     val sc = new SparkContext(conf)
 
-    val indexedRowsA = sc.parallelize(0 until rowsA, parallesim)
+    val indexedRowsA = sc.parallelize(0 until rowsA, parallelism)
       .map(row =>
       IndexedRow(row.toLong, Vectors.dense(BDV.rand[Double](colsA).toArray)))
     val matA = new IndexedRowMatrix(indexedRowsA, rowsA, colsA)
-    val indexedRowsB = sc.parallelize(0 until rowsB, parallesim)
+    val indexedRowsB = sc.parallelize(0 until rowsB, parallelism)
       .map(row =>
       IndexedRow(row.toLong, Vectors.dense(BDV.rand[Double](colsB).toArray)))
     val matB = new IndexedRowMatrix(indexedRowsB, rowsB, colsB)
